9785beb8dd1b32a1260da765bd6c74dd6c5a882b
Kazim110 <kazim.mohammadi110@gmail.com>
2024-02-13 11:52:14 +0430

    add spash screen

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'splash/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -7,5 +8,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "splash#index"
 end
