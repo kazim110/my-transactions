ce6744e075b59d2661ede866a47c951579ddde61
Kazim110 <kazim.mohammadi110@gmail.com>
2024-02-13 12:40:57 +0430

    add name for registeration page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :purchases
   has_many :categories
   has_many :purchase_categories, through: :purchases
