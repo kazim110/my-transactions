3764b103ba2af9ec49d0ccc7204d663fdceffd52
Kazim110 <kazim.mohammadi110@gmail.com>
2024-02-15 22:59:17 +0430

    fix linter error

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,7 +1,7 @@
 class Purchase < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :category
-  
+
   validates :name, presence: true
   validates :amount, presence: true
   validates :category, presence: true
